213f20edad2f8ba4c3b2b3cbf4752d7beb879c36
Serhan <serhankhan0@gmail.com>
2021-09-21 15:29:56 +0300

    Reusable Components and Storyboard Based helper classes added

---
+++ FastNews/Utility/Storyboard/StoryboardBased.swift
@@ -0,0 +1,45 @@
+//
+//  StoryboardBased.swift
+//  FastNews
+//
+//  Created by Serhan Khan on 21/09/2021.
+//
+
+import UIKit
+
+// MARK: Protocol Definition
+
+/// Make your UIViewController subclasses conform to this protocol when:
+///  * they *are* Storyboard-based, and
+///  * this ViewController is the initialViewController of your Storyboard
+///
+/// to be able to instantiate them from the Storyboard in a type-safe manner
+public protocol StoryboardBased: AnyObject {
+  /// The UIStoryboard to use when we want to instantiate this ViewController
+  static var sceneStoryboard: UIStoryboard { get }
+}
+
+// MARK: Default Implementation
+
+public extension StoryboardBased {
+  /// By default, use the storybaord with the same name as the class
+  static var sceneStoryboard: UIStoryboard {
+    return UIStoryboard(name: String(describing: self), bundle: Bundle(for: self))
+  }
+}
+
+// MARK: Support for instantiation from Storyboard
+
+public extension StoryboardBased where Self: UIViewController {
+  /**
+   Create an instance of the ViewController from its associated Storyboard's initialViewController
+
+   - returns: instance of the conforming ViewController
+   */
+  static func instantiate() -> Self {
+    guard let vc = sceneStoryboard.instantiateInitialViewController() as? Self else {
+      fatalError("The initialViewController of '\(sceneStoryboard)' is not of class '\(self)'")
+    }
+    return vc
+  }
+}
