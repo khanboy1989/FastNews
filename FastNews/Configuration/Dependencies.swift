1c1b71646a67f823d4cadb8739ec3847d659395c
Serhan <serhankhan0@gmail.com>
2021-09-22 13:42:54 +0300

    Dependencies Container added

--- FastNews/Configuration/Dependencies.swift
+++ FastNews/Configuration/Dependencies.swift
@@ -19,17 +19,31 @@ class Dependencies {
         self.configure()
     }
     
-    func configure(){
+    func configure() {
         
     }
     
-    func resolve<Service>()->Service {
+    func resolve<Service>() -> Service {
         return resolve(Service.self)
     }
     
-    func resolve<Service>(_ service:Service.Type) -> Service {
+    func resolve<Service>(_ service: Service.Type) -> Service {
         return container.resolve(service)!
     }
     
+    public func resolve<Service, Arg1>(_ service: Service.Type, argument: Arg1) -> Service {
+        return container.resolve(Service.self, argument: argument)!
+    }
+    
+    public func resolve<Service, Arg1, Arg2>(_ service: Service.Type, arguments arg1: Arg1, _ arg2: Arg2) -> Service {
+        return container.resolve(Service.self, arguments: arg1, arg2)!
+    }
+    
+    public func resolve<Service, Arg1, Arg2, Arg3>(_ service: Service.Type,
+                                                   arguments arg1: Arg1,
+                                                   _ arg2: Arg2,
+                                                   _ arg3: Arg3) -> Service {
+        return container.resolve(Service.self, arguments: arg1, arg2, arg3)!
+    }
     
 }
